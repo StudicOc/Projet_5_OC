3f5dee75a629bfaff83f3c57e5eb9fec0990da0a
StudicOC <sabrinaleb48@gmail.com>
2021-11-06 00:11:11 +0100

    Rectructuration du code

--- Front-end/product.js
+++ Front-end/product.js
@@ -1,10 +1,9 @@
-const params = new URLSearchParams(window.location.search);
-// window.location nous renvoit à notre page actuelle
-//objet = ajout de la propriété search
-const id = params.get('id');
-//console.log( window.location.search); Indique la position de la page en cours
 
-// Identification des élements du dom //
+//---CREATION DE L'URL - REDIRECTION DE LA PAGE---//
+const params = new URLSearchParams(window.location.search);
+const id = params.get('id');
+
+
 const optionLenses = document.getElementById('optionLenses');
 let title = document.querySelector('.card-title');
 let description = document.querySelector('.card-text');
@@ -13,7 +12,7 @@ let img = document.querySelector('.card-img-top');
 let addCart = document.querySelector('.add-card');
 
 
-fetch(`http://localhost:3000/api/cameras/${id}`) //Passer le paramêtre dans l'url
+fetch(`http://localhost:3000/api/cameras/${id}`)
   .then(response => response.json())
 
   .then(data => {
@@ -21,36 +20,30 @@ fetch(`http://localhost:3000/api/cameras/${id}`) //Passer le paramêtre dans l'u
 
     title.innerText = data.name;
     description.innerText = data.description;
-    price.innerText = data.price;
+    price.innerText = data.price / 100;
     img.src = data.imageUrl;
 
 
-    for (let lense of data.lenses) {//---- Boucle pour l'itération des options de chaque produit----------
+    for (let lense of data.lenses) {
       optionLenses.innerHTML += `<option value="${lense}">${lense}</option>`;
     }
 
-    // -----------Construction de l'objet product avec les données de l'article----------
-    //---------Récupération de la la séléction du client---------------
+
     let product = {
       title: data.name,
       price: data.price,
-      lense: optionLenses.value,
     }
     console.log(product);
 
-    //----- Ajouter au panier (nav)---------
-
-    // Ajouter au panier
     addCart.addEventListener('click', () => {
-      // Création de l'élément avec le localStorage
+
       localStorage.setItem(data._id, JSON.stringify(product));
       document.querySelector('.cart span').textContent = localStorage.length;
 
       alert("Ajouté au panier");
 
+
     })
-
-
 
   })
   .catch(error => console.log(error))
